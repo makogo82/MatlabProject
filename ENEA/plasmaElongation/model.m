6211389e85da51321ae906ffab48721a024cab31
Mateusz Gospodarczyk <mateuszgospodarczyk@gmail.com>
2015-03-09 10:30:39 +0100

    edit

--- ENEA/plasmaElongation/model.m
+++ ENEA/plasmaElongation/model.m
@@ -1,10 +1,11 @@
 %modello approssimato della posizione verticale
 clear all; clc; close all;
-c_2 = 1E-5;
 c_2H = 1.7E-5;
 c_2L = 0.3E-5;
 c_2 = 1E-5;
 c_3 = 0.8;
+c_4 = c_2*0.001;
+c_5 = 0.4
 Ts = 0.0005;
 
 shots =         [39091   39092  39094  38629  38628    38627  38404  38616 ];
@@ -16,49 +17,56 @@ Ts = 0.5E-3;
 samplingTime = Ts;
 t_start = shots_time(find(shots==shot));
 t_end = shots_endtime(find(shots==shot));
-data = CleanFTUdata_v6(t_start,t_end,Ts,shot,1);
-t=data.time;
+
+dataAll = CleanFTUdata_v6(0.2,1.2,Ts,shot,1);
+t = [t_start:Ts:t_end];
+
+data.IPLmis = interp1(dataAll.time,dataAll.IPLmis, t);
+data.Hmis   = interp1(dataAll.time,dataAll.Hmis,   t);
+data.Fmis   = interp1(dataAll.time,dataAll.Fmis,   t);
+data.dez    = interp1(dataAll.time,dataAll.dez,    t);
+
 
 c_dez = 2;
 d_dez = 9;
 y = data.dez/((1.5e-4) * (-1.0));
 dy = mypseudo_derivative(t,y,c_dez,d_dez);
 
-
-
 x1(1)=y(1);
-x1L(1) =y(1);
-x1H(1) =y(1);
-x2(1) = dy(1);
-x2L(1) =dy(1);
-x2H(1) =dy(1);
+x1L(1) = y(1);
+x1H(1) = y(1);
+x2(1)  =  dy(1);
+x2L(1) = dy(1);
+x2H(1) = dy(1);
 %%
 time(1) = t(1);
 for h=1:length(t)-1
     IPLmis = data.IPLmis(h);
     Hmis = data.Hmis(h);
     Fmis = data.Fmis(h);
-    t1 = c_2*IPLmis*Hmis*(1/(c_3-1.5*(x1(h))) - 1/(-c_3-1.5*(x1(h))) );
+    t1 =   c_2*IPLmis*Hmis*(1/(c_3-1.5*(x1(h)))    - 1/(-c_3-1.5*(x1(h))) );
     t1L = c_2L*IPLmis*Hmis*(1/(c_3-1.5*(x1L(h))) - 1/(-c_3-1.5*(x1L(h))) );
     t1H = c_2H*IPLmis*Hmis*(1/(c_3-1.5*(x1H(h))) - 1/(-c_3-1.5*(x1H(h))) );
-    c_4 = c_2*0.001;
-    c_5 = 0.4; 
-    t2 = -c_4*Fmis*IPLmis*(2*( 1/(c_5-1.5*(x1(h))) + 1/(-c_5-1.5*(x1(h)))))^2';
-    t2L = -c_4*Fmis*IPLmis*(2*( 1/(c_5-1.5*(x1L(h))) + 1/(-c_5-1.5*(x1L(h)))))^2';
-    t2H = -c_4*Fmis*IPLmis*(2*( 1/(c_5-1.5*(x1H(h))) + 1/(-c_5-1.5*(x1H(h)))))^2';
+    
+    t2 =  -c_4*Fmis*IPLmis*(2*( 1/(c_5-1.5*(x1(h))) + 1/(-c_5-1.5*(x1(h)))))^2;
+    t2L = -c_4*Fmis*IPLmis*(2*( 1/(c_5-1.5*(x1L(h))) + 1/(-c_5-1.5*(x1L(h)))))^2;
+    t2H = -c_4*Fmis*IPLmis*(2*( 1/(c_5-1.5*(x1H(h))) + 1/(-c_5-1.5*(x1H(h)))))^2;
     %%
 
     % EULERO (funzioni)
-    x2(h+1) = x2(h) + Ts*(t1+t2);
+    %velocity
+    x2(h+1) =  x2(h) +  Ts*(t1+t2);
     x2L(h+1) = x2L(h) + Ts*(t1L+t2L);
     x2H(h+1) = x2H(h) + Ts*(t1H+t2H);
-    x1(h+1) = x1(h) + Ts*x2(h);
+    %position
+    x1(h+1) = x1(h)   + Ts*x2(h);
     x1L(h+1) = x1L(h) + Ts*x2L(h);
     x1H(h+1) = x1H(h) + Ts*x2H(h);
+    %
     time(h+1) = time(h)+Ts;
 
 end
-%%
+
 close all
 figure('Name','velocity/position')
 subplot(2,1,1)
